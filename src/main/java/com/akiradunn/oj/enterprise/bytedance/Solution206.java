3c2417fab36267036a7f99e42afa550e23d071d8
akiradunn@163.com <akiradunn@163.com>
2021-05-19 23:13:14 +0800

    LeetCode2. 两数相加

--- src/main/java/com/akiradunn/oj/enterprise/bytedance/Solution206.java
+++ src/main/java/com/akiradunn/oj/enterprise/bytedance/Solution206.java
@@ -48,4 +48,17 @@ public class Solution206 {
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
+
+    class Solution2 {
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            while(head != null){
+                ListNode next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+            return prev;
+        }
+    }
 }
