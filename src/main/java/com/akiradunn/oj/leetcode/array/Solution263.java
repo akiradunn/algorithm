df03a66d1980a5c125f71da15d49d34efcb2c89c
axi <axi@qunhemail.com>
2021-07-05 21:23:40 +0800

    LeetCode263. 丑数

---
+++ src/main/java/com/akiradunn/oj/leetcode/array/Solution263.java
@@ -0,0 +1,50 @@
+package com.akiradunn.oj.leetcode.array;
+//263. 丑数
+//给你一个整数 n ，请你判断 n 是否为 丑数 。如果是，返回 true ；否则，返回 false 。
+//
+//丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+//
+//
+//
+//示例 1：
+//
+//输入：n = 6
+//输出：true
+//解释：6 = 2 × 3
+//示例 2：
+//
+//输入：n = 8
+//输出：true
+//解释：8 = 2 × 2 × 2
+//示例 3：
+//
+//输入：n = 14
+//输出：false
+//解释：14 不是丑数，因为它包含了另外一个质因数 7 。
+//示例 4：
+//
+//输入：n = 1
+//输出：true
+//解释：1 通常被视为丑数。
+//
+//
+//提示：
+//
+//-231 <= n <= 231 - 1
+//通过次数98,629提交次数191,547
+public class Solution263 {
+    class Solution {
+        public boolean isUgly(int n) {
+            if (n <= 0) {
+                return false;
+            }
+            int[] factors = {2, 3, 5};
+            for (int factor : factors) {
+                while (n % factor == 0) {
+                    n /= factor;
+                }
+            }
+            return n == 1;
+        }
+    }
+}
