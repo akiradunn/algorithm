64b7a3ad3aaf78f55eb8e35f08a5453d6d15e167
duanzengliang <duanzengliang@codemao.cn>
2025-11-18 22:02:33 +0800

    leetcode 96. 不同的二叉搜索树 v2

--- src/main/java/com/akiradunn/oj/leetcode/tree/Solution96.java
+++ src/main/java/com/akiradunn/oj/leetcode/tree/Solution96.java
@@ -34,7 +34,11 @@ import java.util.Map;
 class Solution96 {
 
     /**
-     * 暴露问题: 想的点都很零碎, 都没有串连起来
+     * 暴露问题: 想的点都很零碎, 没有串连起来
+     * 1. 第一要务，不是思考套什么方法，而是看懂题目，找规律，题目的示例是怎么程序得出来的
+     * 2. 方法入参、出参、边界条件、结束条件
+     * 3. 主函数调用逻辑是怎样的
+     * 4。题目给到的条件，一定要利用起来，能降低时间复杂度
      */
     public int numTrees(int n) {
         List<Integer> selectiveNumbers = new ArrayList<>();
@@ -80,5 +84,29 @@ class Solution96 {
         }
 
         return result;
+    }
+
+    public static int numTreesV2(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        // dp[n] = f(1) + f(2) + f(3) + ... f(n)
+        // f(n) 代表 n 为顶点的二叉搜索树的总数
+        for (int i=1; i<=n; i++) {
+            int sum = 0;
+            // i 当顶点的时候, 讨论左子树总节点树的情况
+            for (int j=1; j<=i; j++) {
+                // 从左子树为0个节点, 到左子树有 i-1 个节点, 右子树节点数从 i-1 递减为 0
+                // 左子树不能有 i 个节点, 因为 i 已经拿来当顶点了
+                sum = sum + dp[j-1] * dp[i-j];
+            }
+            dp[i] = sum;
+        }
+
+        return dp[n];
     }
 }
